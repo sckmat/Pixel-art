6ac87bd05460c0d7cb922bd88c9ad47f0b71e004
Vladislav Yegorov <77292451+sckmat@users.noreply.github.com>
2021-11-17 14:46:04 +0600

    Added source code

---
+++ filter.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import numpy as np
+
+
+class ConvertImage:
+
+    def __init__(self, image, size, gradation):
+        self.image = image
+        self.size = size
+        self.step = 255 // gradation
+
+    def convert_image(self):
+        height = len(self.image)
+        width = len(self.image[1])
+        for i in range(0, height, self.size):
+            for j in range(0, width, self.size):
+                medium_brightness = self.get_medium_brightness(i, j)
+                self.set_grayscale(medium_brightness, i, j)
+        return Image.fromarray(self.image)
+
+    def set_grayscale(self, medium_brightness, i, j):
+        self.image[i:i + self.size, j:j + self.size] = int(medium_brightness // self.step) * self.step / 3
+
+    def get_medium_brightness(self, i, j):
+        return int((self.image[i:i + self.size, j:j + self.size].sum()) // self.size ** 2)
+
+
+original_image = Image.open(input("Введите имя исходного изображения:"))
+pixels = np.array(original_image)
+s = int(input("Введите размер блока (целое положительное число):"))
+grad = int(input('Введите количество оттенков (целое положительное число):'))
+result = ConvertImage(pixels, s, grad).convert_image()
+result.save((input("Введите имя изображения, в которое запишется результат:")))
